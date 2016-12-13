04437768e93c19784e2ea2a74feed9ec5f5f1440
Dima <dimadidyk0@gmail.com>
2016-12-13 16:58:48 +0200

    1

---
+++ js/vvr-js/main.js
@@ -0,0 +1,7 @@
+alert(document.getElementByClassName('public__video-list').childNodes.length);
+
+    // способ 2:
+    alert(document.getElementClassName('public__video-item').getElementsByTagName('div').length);
+
+
+    alert("a");
