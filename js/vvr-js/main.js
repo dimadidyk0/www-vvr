8cef2ae76753d5eed36d5a8f890b850baaae2b96
Dima <dimadidyk0@gmail.com>
2016-12-13 17:38:36 +0200

    2

--- js/vvr-js/main.js
+++ js/vvr-js/main.js
@@ -1,7 +1,3 @@
-alert(document.getElementByClassName('public__video-list').childNodes.length);
-
-    // способ 2:
-    alert(document.getElementClassName('public__video-item').getElementsByTagName('div').length);
-
-
-    alert("a");
+var mySiema = new Siema();
+			document.querySelector('.js-prev1').addEventListener('click', function() {mySiema.prev()});
+			document.querySelector('.js-next1').addEventListener('click', function() {mySiema.next()});
